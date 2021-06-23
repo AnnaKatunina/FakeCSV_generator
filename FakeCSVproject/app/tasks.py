e49c72693de7a536b982e8bf17577eb91a0ff68c
Anna Katunina <leznevoanna@gmail.com>
2021-06-23 17:22:38 +0100

    added cloudinary

--- FakeCSVproject/app/tasks.py
+++ FakeCSVproject/app/tasks.py
@@ -30,7 +30,7 @@ def generate_csv_file(schema_id, number_rows, dataset_id):
             for column in columns:
                 fake_data.append(get_fake_data(column.type, column.From, column.To))
             writer.writerow(fake_data)
-        uploaded_file = cloudinary.uploader.upload(csv_file.name, resource_type='auto', public_id=csv_file.name)
+        uploaded_file = cloudinary.uploader.upload(csv_file.name, resource_type='auto')
         logger.info(uploaded_file)
         dataset.csv_file = uploaded_file['secure_url']
         dataset.status = Dataset.Status.READY
